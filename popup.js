843914645b3556f4a616aa0c55dc61f4d7bab288
Bah Nigel <bah.nigel@yahoo.com>
2024-04-03 10:25:12 +0100

    my first commit

---
+++ popup.js
@@ -0,0 +1,12 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var getTabTitleButton = document.getElementById('getTabTitle');
+    var tabTitleDisplay = document.getElementById('tabTitle');
+  
+    getTabTitleButton.addEventListener('click', function() {
+      chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
+        var currentTab = tabs[0];
+        tabTitleDisplay.textContent = currentTab.title;
+      });
+    });
+  });
+  
